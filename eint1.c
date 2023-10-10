42503f96ec98c7e89978c08ec2503e99e320677e
Julieta Bernaus <76222131+jberlar@users.noreply.github.com>
2023-10-10 13:25:29 -0300

    1.1

---
+++ eint1.c
@@ -0,0 +1,64 @@
+#ifdef __USE_CMSIS
+#include "LPC17xx.h"
+#endif
+
+#define LED (1<<22)
+
+void configGPIO(void);
+void delay(uint32_t tiempo);
+void confIntExt(void);
+void EINT3_IRQHandler(void);
+uint8_t inte;
+
+int main(void) {
+
+		uint32_t relojCpu = SystemCoreClock;
+
+		configGPIO();
+		confIntExt();
+
+		while(1){
+			if(inte%2){
+				tiempo = 1000000;
+			}
+			else{
+				tiempo= 100000;
+			}
+
+			LPC_GPIO0->FIOCLR |= (LED);
+			delay(tiempo);
+			LPC_GPIO0->FIOSET |= (LED);
+			delay(tiempo);
+		}
+
+
+    return 0 ;
+}
+
+void configGPIO(void){
+
+	LPC_GPIO0->FIODIR |= (LED);
+	return;
+}
+
+void delay(uint32_t tiempo){
+	uint32_t counter;
+	for(counter = 0; counter < tiempo; counter++){};
+	return;
+}
+
+//RE Int @ P2.10
+void confIntExt(void){
+	LPC_PINCON->PINSEL4 |= (1<<20);
+	LPC_SC->EXTINT |= 1;
+	LPC_SC->EXTMODE |= 1;
+	LPC_SC->EXTPOLAR |= 1;
+	NVIC_Enable(EINT0_IRQn);
+	return;
+}
+
+void EINT0_IRQHandler(void){
+
+	inte++;
+	LPC_SC->EXTINT |= 1;
+}
